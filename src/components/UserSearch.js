321757693b3d3a39b4c2a869e2c00f3940b4c147
kasib-94 <jakczarski@gmail.com>
2022-10-20 00:29:07 +0200

    addding form that search album by id in UserSearch.js

--- src/components/UserSearch.js
+++ src/components/UserSearch.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {useNavigate} from "react-router-dom";
 import Card from "./Card";
 
@@ -6,6 +6,7 @@ function UserSearch() {
     const navigate = useNavigate();
     const [username, setUsername] = useState('')
     const [card, setCard] = useState('')
+    const [album, setAlbum] = useState(0)
     const [id, setId] = useState('')
     const handleSubmit = async (e) => {
         e.preventDefault()
@@ -33,10 +34,22 @@ function UserSearch() {
 
     }
 
+    useEffect(() => {
+        console.log(album)
+        console.log(typeof (album))
+
+        if (album !== 0 && Object.keys(album).length !== 0) {
+            navigate(`/cardlist/${album.id}`)
+        } else if (album !== 0) {
+            document.getElementById("errorAlbum").className = "py-5 px-5";
+        }
+    }, [album]); //
+
     function showSinglePhoto() {
 
         document.getElementById("serach-photo").className = "py-5 px-5";
     }
+
 
     const handleSubmitPhoto = async (e) => {
         e.preventDefault()
@@ -49,13 +62,23 @@ function UserSearch() {
             showSinglePhoto();
 
         } else {
-            console.log(typeof (document.getElementById("photoId").value))
+            document.getElementById("serach-photo").className = "hidden";
+            console.log(typeof (document.getElementById("errorPhoto").className = "py-5 px-5"))
         }
 
     }
     const handleChange = (e) => {
         document.getElementById("error").className = "hidden";
         setUsername(e.target.value.toLowerCase());
+    }
+    const handleAlbumId = (e) => {
+        e.preventDefault()
+        console.log(parseInt(document.getElementById("albumId").value))
+        fetch(`https://jsonplaceholder.typicode.com/albums/${parseInt(document.getElementById("albumId").value)}`)
+            .then(response => response.json())
+            .then(json => setAlbum(json))
+
+
     }
 
 
@@ -88,7 +111,9 @@ function UserSearch() {
                 <h1 className="inline-flex pr-8">Find Photo:</h1>
                 <input
                     className="shadow appearance-none border rounded w-1/3 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    id="photoId" type="text" placeholder="Photo Id"/>
+                    id="photoId" type="text" placeholder="Photo Id"
+
+                />
                 <button type="submit"
                         className="mx-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-5 border border-blue-700 rounded"
                 >Submit
@@ -104,6 +129,28 @@ function UserSearch() {
                     </div>
                 </div>
             </form>
+
+            <form onSubmit={handleAlbumId} className="row-auto mb-10">
+                <h1 className="inline-flex pr-8">Find Album:</h1>
+                <input
+                    className="shadow appearance-none border rounded w-1/3 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="albumId" type="text" placeholder="Type Album Id !"/>
+                <button type="submit"
+                        className="mx-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-5 border border-blue-700 rounded"
+                >Submit
+                </button>
+                <div className="hidden" id="errorAlbum">
+                    <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+                         role="alert">
+                        <strong className="font-bold">Holy smokes ! </strong>
+                        <span className="block sm:inline "
+
+                        >Cannot find album ! , example id : 1-100</span>
+
+                    </div>
+                </div>
+            </form>
+
             <div className="hidden" id="serach-photo">
 
                 <h1> Photo with id : {card.id}</h1>
